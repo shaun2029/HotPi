4f2899535304a29aa97ce8e11599a5320166ff1a
Shaun2029 <shauns2029@gmail.com>
2016-05-02 22:12:13 +0100

    Radiogaga update.

--- pi-build.sh
+++ pi-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export CCACHE_DIR=/media/Data/Temp/ccache-openelec-pi
-export CONCURRENCY_MAKE_LEVEL=10
-PROJECT=RPi ARCH=arm make release
+export CONCURRENCY_MAKE_LEVEL=7
+PROJECT=RPi ARCH=arm nice -n 15 make release
 
