7f8b13d418ec857cdd244b8c42c0a470b1c2b8c7
Shaun2029 <shauns2029@gmail.com>
2018-02-15 20:30:45 +0000

    Simplified snapclient dependencies.

--- packages/hotpi/files/usr/config/autostart.sh
+++ packages/hotpi/files/usr/config/autostart.sh
@@ -1,6 +1,14 @@
 # Load driver to direct non video audio out the HDMI such as Radiogaga.
 # Use if not using an attached USB audio output device.
 (
-modprobe snd_bcm2835;
+#modprobe snd_bcm2835;
+/usr/lib/libreelec/lircd_helper add lirc0:default:/storage/.config/lircd.conf;
+# Set the startup volume.
+amixer set Speaker 20%;
+systemctl restart radiogaga;
+# Restart Kodi if less than 100MB of RAM is free.
+/usr/bin/watch-kodi 100;
+# Play soft sound every 15 minutes to keep speaker alive.
+/usr/bin/keep-audio-awake "/usr/share/sounds/chirp.wav";
 ) &
 
