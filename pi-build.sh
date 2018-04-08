2c2f841fc2e6caa4401bf878fedd8126a69e7928
Shaun2029 <shauns2029@gmail.com>
2018-04-08 09:56:34 +0100

    Updated build lauch scripts to detect cpu threads.

--- pi-build.sh
+++ pi-build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+if [ $1 = "" ]; then
+  CPU=`grep -c ^processor /proc/cpuinfo`
+else
+  CU=$1
+fi
+
 export CCACHE_DIR=/media/Data/Temp/ccache-openelec-pi
-export CONCURRENCY_MAKE_LEVEL=9
+export CONCURRENCY_MAKE_LEVEL=$CPU
 PROJECT=RPi ARCH=arm nice -n 15 make release
 
--- pi2-build.sh
+++ pi2-build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 echo "Building for ARM ..."
+!/bin/sh
+if [ $1 = "" ]; then
+  CPU=`grep -c ^processor /proc/cpuinfo`
+else
+  CU=$1
+fi
+
 export CCACHE_DIR=/media/Data/Temp/ccache-openelec-pi
-export CONCURRENCY_MAKE_LEVEL=9
-PROJECT=RPi2 ARCH=arm nice -n 5 make release
+export CONCURRENCY_MAKE_LEVEL=$CPU
+PROJECT=RPi2 ARCH=arm nice -n 15 make release
 
